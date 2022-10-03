0eb7db895e4dbd37a3cfbd3554ee9c44e9625f08
mrcsporto <mrcsporto@gmail.com>
2022-10-03 08:39:08 -0300

    texto

---
+++ eventos.js
@@ -0,0 +1,11 @@
+//let texto ='0123456789';
+//let texto ='mergulhando nas profundezas';
+
+//console.log(texto.toUpperCase().indexOf("g")) = -1
+//let = 'sofia21 pcb'
+//console.log(texto.split(",")[0].join)
+
+let texto = "sofia Manzano";
+
+console.log(texto.split(" ")[0]+texto.indexOf("f")+texto.indexOf("o"))
+//console.log(texto.indexOf(0,5)+texto.indexOf(f)+texto.indexOf(o))
