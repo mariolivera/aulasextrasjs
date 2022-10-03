f489d06faba22a71cb898039f59f79ca3aa48c3c
mrcsporto <mrcsporto@gmail.com>
2022-10-03 09:44:33 -0300

    +coisa

--- eventos.js
+++ eventos.js
@@ -1,11 +1,22 @@
-//let texto ='0123456789';
+/*//let texto ='0123456789';
 //let texto ='mergulhando nas profundezas';
 
 //console.log(texto.toUpperCase().indexOf("g")) = -1
 //let = 'sofia21 pcb'
 //console.log(texto.split(",")[0].join)
 
-let texto = "sofia Manzano";
+//let texto = "sofia Manzano";
 
-console.log(texto.split(" ")[0]+texto.indexOf("f")+texto.indexOf("o")) //esse caso manipulanos uma string para da um outro resultado.gigi
+//console.log(texto.split(" ")[0]+texto.indexOf("f")+texto.indexOf("o")) //esse caso manipulanos uma string para da um outro resultado.
 //console.log(texto.indexOf(0,5)+texto.indexOf(f)+texto.indexOf(o))
+
+//let texto = "sofia Manzano";
+
+//console.log(!"")//valores verdadeiros e falsos
+
+//usar para checar 
+//let texto= "pessoa.idade"
+//console.log("pessoa.idade", texto.split() );*/
+
+let x = "sofia"
+console.log(x !== "carla" ? "true":"false");
